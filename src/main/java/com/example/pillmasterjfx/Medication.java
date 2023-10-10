9caa208a8ecebe4e4019468d112cb7418fbbf054
TopCrunch <100889952+TopCrunch@users.noreply.github.com>
2023-10-10 11:16:30 -0700

    Medication data class created

---
+++ src/main/java/com/example/pillmasterjfx/Medication.java
@@ -0,0 +1,42 @@
+package com.example.pillmasterjfx;
+
+public class Medication {
+    private final String name;
+    private int count;
+    public enum Schedule {
+        DAILY,
+        TWICE,
+        THRICE
+    }
+    private Schedule schedule;
+
+    public Medication(String name, int count, Schedule schedule) {
+        this.name = name;
+        this.count = count;
+        this.schedule = schedule;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void popCount() {
+        count--;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public Schedule getSchedule() {
+        return schedule;
+    }
+
+    public void setSchedule(Schedule schedule) {
+        this.schedule = schedule;
+    }
+}
