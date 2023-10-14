6acac66e5600f15541e6f63578828b94c4cae65f
TopCrunch <100889952+TopCrunch@users.noreply.github.com>
2023-10-14 13:27:04 -0700

    added dependencies for HttpClient and org.json. Also removed sample code from NetworkClient.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,6 +7,8 @@ module com.example.pillmasterjfx {
     requires net.synedra.validatorfx;
     requires org.kordamp.bootstrapfx.core;
     requires jssc;
+    requires org.apache.httpcomponents.httpclient;
+    requires org.json;
 
     opens com.example.pillmasterjfx to javafx.fxml;
     exports com.example.pillmasterjfx;
